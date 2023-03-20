06dea93c9920e15846b9607d71db1b582a5f62ea
lasha <lasha81@gmail.com>
2023-03-20 17:13:47 +0400

    fixed some bugs

--- src/test.py
+++ src/test.py
@@ -1,5 +1,19 @@
 import chess
 import azg_chess.players as players
+from functools import partial
+from azg.Arena import Arena
+from azg_chess.game import (
+    BLACK_PLAYER,
+    BOARD_DIMENSIONS,
+    INVALID_MOVE,
+    NUM_PIECES,
+    NUM_SQUARES,
+    VALID_MOVE,
+    WHITE_PLAYER,
+    ChessGame,
+    PlayerID,
+)
+
 
 
 
@@ -10,29 +24,39 @@ import azg_chess.players as players
 b = chess.Board()
 #b.push(chess.Move.from_uci("e2e4"))
 
-p1 = players.StockfishChessPlayer(engine_path='/usr/games/stockfish')
-p2 = players.GeochriPlayer(nn_path='model_data/iter4_100.tar')
+#p_stockfish_1350 = players.StockfishChessPlayer(engine_path='/usr/games/stockfish',engine_elo=1350)
+p_random = players.RandomChessPlayer()
+p_geo1 = players.GeochriPlayer(parameters_file='model_data/iter_1.tar',mcts_steps_per_move = 100)
+p_geo2 = players.GeochriPlayer(parameters_file='model_data/iter_2.tar',mcts_steps_per_move = 100)
+p_geo3 = players.GeochriPlayer(parameters_file='model_data/iter_3.tar',mcts_steps_per_move = 100)
+p_geo4 = players.GeochriPlayer(parameters_file='model_data/iter_4.tar',mcts_steps_per_move = 100)
+p_geo5 = players.GeochriPlayer(parameters_file='model_data/iter_5.tar',mcts_steps_per_move = 100)
 
-print(b)
-
-
-while not (b.is_game_over()):
-    m = p1.choose_move(b)
-    print(m.uci())
-    b.push(m)
-    print(b)
-    print("-----")
-
-    if b.is_game_over():
-        break
-    m = p2.choose_move(b)
-    print(m.uci())
-    b.push(m)
-    print(b)
-    print("-----")
+TOTAL_GAMES = 5
 
 
+def play_players(player1, player2, board, num_games = TOTAL_GAMES):
+    outcomes = []
+    for current_game in range(num_games):
+        b.reset()
+        print(f"========================= GAME {current_game+1} of {num_games}")
+        while not (b.is_game_over()):
+            m = player1.choose_move(b)
+            print(m.uci())
+            b.push(m)
+            print(b)
+            if b.is_game_over():
+                break
+            m = player2.choose_move(b.mirror())
+            m = chess.Move(chess.square_mirror(m.from_square), chess.square_mirror(m.to_square), m.promotion)
+            print(m.uci())
+            b.push(m)
+            print(b)
+            print("-----")
 
+        print(b.outcome().result())
+        outcomes.append(b.outcome().result())
 
+    print(outcomes)
 
-
+play_players(p_random,p_geo5, b, 3)
